26fb8a32f96ac81484cd7e2abb3b631f3528818d
pitekkate <kangristiktok@gmail.com>
2025-07-22 08:15:21 +0700

    Update RunR8.java

--- appinventor/buildserver/src/com/google/appinventor/buildserver/tasks/android/RunR8.java
+++ appinventor/buildserver/src/com/google/appinventor/buildserver/tasks/android/RunR8.java
@@ -19,7 +19,7 @@ import java.util.*;
 @BuildType(aab = true, apk = true)
 public class RunR8 extends DexTask implements AndroidTask {
 
-  private static final boolean USE_D8_PROGUARD_RULES = true;
+  private static final boolean USE_PROGUARD_MAIN_DEX_HINTS = true;
 
   @Override
   public TaskResult execute(AndroidCompilerContext context) {
@@ -27,8 +27,10 @@ public class RunR8 extends DexTask implements AndroidTask {
     final List<File> inputs = new ArrayList<>();
 
     try {
+      // Collect user .class files
       recordForMainDex(context.getPaths().getClassesDir(), mainDexClasses);
 
+      // Add runtime JARs
       inputs.add(preDexLibrary(context, recordForMainDex(
           new File(context.getResources().getSimpleAndroidRuntimeJar()), mainDexClasses)));
       inputs.add(preDexLibrary(context, recordForMainDex(
@@ -54,6 +56,7 @@ public class RunR8 extends DexTask implements AndroidTask {
         inputs.add(preDexLibrary(context, new File(lib)));
       }
 
+      // Extension libraries
       Set<String> addedExtJars = new HashSet<>();
       for (String type : context.getExtCompTypes()) {
         String sourcePath = ExecutorUtils.getExtCompDirPath(type, context.getProject(),
@@ -64,12 +67,8 @@ public class RunR8 extends DexTask implements AndroidTask {
         }
       }
 
-      Files.walkFileTree(context.getPaths().getClassesDir().toPath(), new FileVisitor<Path>() {
-        @Override
-        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
-          return FileVisitResult.CONTINUE;
-        }
-
+      // User compiled classes
+      Files.walkFileTree(context.getPaths().getClassesDir().toPath(), new SimpleFileVisitor<Path>() {
         @Override
         public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
           if (file.toString().endsWith(".class")) {
@@ -77,21 +76,13 @@ public class RunR8 extends DexTask implements AndroidTask {
           }
           return FileVisitResult.CONTINUE;
         }
-
-        @Override
-        public FileVisitResult visitFileFailed(Path file, IOException exc) {
-          return FileVisitResult.CONTINUE;
-        }
-
-        @Override
-        public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
-          return FileVisitResult.CONTINUE;
-        }
       });
 
-      // Gunakan aturan main-dex hanya jika min-api < 21
+      // Main dex rules only for min-api < 21
       int minSdk = AndroidBuildUtils.computeMinSdk(context);
-      if (USE_D8_PROGUARD_RULES) {
+      boolean useMainDexRules = minSdk < 21;
+
+      if (USE_PROGUARD_MAIN_DEX_HINTS && useMainDexRules) {
         mainDexClasses.clear();
         mainDexClasses.add("com.google.appinventor.components.runtime.*");
         mainDexClasses.add("com.google.appinventor.components.runtime.**.*");
@@ -101,143 +92,162 @@ public class RunR8 extends DexTask implements AndroidTask {
         mainDexClasses.add("androidx.collection.*");
         mainDexClasses.add("androidx.vectordrawable.**.*");
         mainDexClasses.add(context.getProject().getMainClass());
+      } else if (USE_PROGUARD_MAIN_DEX_HINTS) {
+        context.getReporter().info("Skipping --main-dex-rules: min-api " + minSdk + " >= 21");
       }
 
-      // Hanya gunakan --main-dex-rules jika min-api < 21
-      boolean useMainDexRules = minSdk < 21 && !mainDexClasses.isEmpty();
-
-      if (!runR8(context, inputs, useMainDexRules ? mainDexClasses : null)) {
-        return TaskResult.generateError("R8 failed.");
+      // Final R8 execution
+      if (!runR8Final(context, inputs, useMainDexRules ? mainDexClasses : null)) {
+        return TaskResult.generateError("R8 final pass failed.");
       }
 
-      // Cari classes.dex di tmp/
-      File[] dexFiles = context.getPaths().getTmpDir().listFiles((dir, name) -> name.endsWith(".dex"));
-      if (dexFiles == null || dexFiles.length == 0) {
-        throw new FileNotFoundException("R8 did not produce any .dex files");
+      // Ensure classes.dex exists
+      File outputDex = new File(context.getPaths().getTmpDir(), "classes.dex");
+      if (!outputDex.exists()) {
+        return TaskResult.generateError("R8 did not produce classes.dex");
       }
 
-      Collections.addAll(context.getResources().getDexFiles(), dexFiles);
+      // Add to resources for RunApkBuilder
+      context.getResources().getDexFiles().add(outputDex);
+
       return TaskResult.generateSuccess();
 
     } catch (IOException e) {
+      context.getReporter().error("Exception in RunR8: " + e.getMessage());
       return TaskResult.generateError(e);
     }
   }
 
-  private static boolean runR8(AndroidCompilerContext context, Collection<File> inputs,
-                               Set<String> mainDexClasses) throws IOException {
-    return runR8(context, inputs, mainDexClasses, context.getPaths().getTmpDir().getAbsolutePath(), null);
-  }
+  /**
+   * Runs final R8 step.
+   */
+  private boolean runR8Final(AndroidCompilerContext context, Collection<File> inputs,
+                             Set<String> mainDexClasses) throws IOException {
+    List<String> cmd = new ArrayList<>();
+    cmd.add("java");
+    cmd.add("-Xmx" + context.getChildProcessRam() + "M");
+    cmd.add("-Xss8m");
+    cmd.add("-cp");
+    cmd.add(context.getResources().getR8Jar());
+    cmd.add("com.android.tools.r8.R8");
 
-  private static boolean runR8(AndroidCompilerContext context, Collection<File> inputs,
-                               Set<String> mainDexClasses, String outputDir, String intermediateFileName)
-      throws IOException {
+    // R8 Options
+    cmd.add("--release");
+    cmd.add("--lib");
+    cmd.add(context.getResources().getAndroidRuntime());
+    cmd.add("--output");
+    cmd.add(context.getPaths().getTmpDir().getAbsolutePath());
+    cmd.add("--min-api");
+    cmd.add(String.valueOf(AndroidBuildUtils.computeMinSdk(context)));
+    cmd.add("--no-desugaring");
+    cmd.add("--no-minification");
 
-    List<String> arguments = new ArrayList<>();
-    List<String> javaArgs = new ArrayList<>();
+    // Classpath
+    cmd.add("--classpath");
+    cmd.add(context.getPaths().getClassesDir().getAbsolutePath());
 
-    arguments.add("java");
-    javaArgs.add("-Xmx" + context.getChildProcessRam() + "M");
-    javaArgs.add("-Xss8m");
-    javaArgs.add("-cp");
-    javaArgs.add(context.getResources().getR8Jar());
-    javaArgs.add("com.android.tools.r8.R8");
-
-    javaArgs.add("--release");
-    javaArgs.add("--lib");
-    javaArgs.add(context.getResources().getAndroidRuntime());
-    javaArgs.add("--output");
-    javaArgs.add(outputDir);
-    javaArgs.add("--min-api");
-    javaArgs.add(Integer.toString(AndroidBuildUtils.computeMinSdk(context)));
-    javaArgs.add("--no-desugaring");
-    javaArgs.add("--no-minification");
-
-    if (intermediateFileName == null) {
-      javaArgs.add("--classpath");
-      javaArgs.add(context.getPaths().getClassesDir().getAbsolutePath());
-    }
-
-    // 🔥 HANYA TAMBAHKAN --main-dex-rules JIKA mainDexClasses != null DAN min-api < 21
+    // Only add --main-dex-rules if min-api < 21
     if (mainDexClasses != null && !mainDexClasses.isEmpty()) {
-      javaArgs.add("--main-dex-rules");
-      String rulesFile = writeClassRules(context.getPaths().getClassesDir(), mainDexClasses);
-      context.getReporter().info("Using main dex rules: " + rulesFile);
-      javaArgs.add(rulesFile);
+      File rulesFile = writeClassRulesToFile(context.getPaths().getTmpDir(), mainDexClasses);
+      cmd.add("--main-dex-rules");
+      cmd.add(rulesFile.getAbsolutePath());
+      context.getReporter().info("Using main dex rules: " + rulesFile.getName());
     }
 
+    // Input JARs/Dirs
     for (File input : inputs) {
-      javaArgs.add(input.getAbsolutePath());
+      cmd.add(input.getAbsolutePath());
     }
 
-    context.getReporter().info("Executing R8 with arguments: " + String.join(" ", javaArgs));
-
-    File javaArgsFile = new File(context.getPaths().getTmpDir(), "r8arguments.txt");
-    try (PrintStream ps = new PrintStream(new FileOutputStream(javaArgsFile))) {
-      for (String arg : javaArgs) {
-        ps.println(arg);
-      }
+    // Log command
+    context.getReporter().info("Executing R8:");
+    for (String arg : cmd) {
+      context.getReporter().info("  " + arg);
     }
 
-    arguments.add("@" + javaArgsFile.getAbsolutePath());
-
-    ByteArrayOutputStream outputBuffer = new ByteArrayOutputStream();
-    ByteArrayOutputStream errorBuffer = new ByteArrayOutputStream();
-    PrintStream outStream = new PrintStream(outputBuffer);
-    PrintStream errStream = new PrintStream(errorBuffer);
+    ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+    ByteArrayOutputStream errStream = new ByteArrayOutputStream();
 
     synchronized (context.getResources().getSyncKawaOrDx()) {
-      boolean result = Execution.execute(
+      boolean success = Execution.execute(
           context.getPaths().getTmpDir(),
-          arguments.toArray(new String[0]),
-          outStream, errStream, Execution.Timeout.LONG
+          cmd.toArray(new String[0]),
+          new PrintStream(outStream),
+          new PrintStream(errStream),
+          Execution.Timeout.LONG
       );
 
-      String outputStr = outputBuffer.toString();
-      String errorStr = errorBuffer.toString();
+      String output = outStream.toString();
+      String error = errStream.toString();
 
-      if (!outputStr.isEmpty()) {
-        context.getReporter().info("R8 Output:\n" + outputStr);
-      }
-      if (!errorStr.isEmpty()) {
-        context.getReporter().error("R8 Error:\n" + errorStr);
-      }
+      if (!output.isEmpty()) context.getReporter().info("R8 Output:\n" + output);
+      if (!error.isEmpty()) context.getReporter().error("R8 Error:\n" + error);
 
-      if (!result) {
-        return false;
-      }
+      return success;
     }
-
-    // Pindahkan classes.dex jika diperlukan (pre-dex)
-    if (intermediateFileName != null) {
-      Path source = FileSystems.getDefault().getPath(outputDir, "classes.dex");
-      Path target = FileSystems.getDefault().getPath(outputDir, intermediateFileName);
-      context.getReporter().info("Moving " + source + " to " + target);
-      Files.move(source, target, StandardCopyOption.REPLACE_EXISTING);
-    }
-
-    return true;
   }
 
-  private static File preDexLibrary(AndroidCompilerContext context, File input) throws IOException {
+  /**
+   * Pre-dex a library using R8.
+   */
+  private File preDexLibrary(AndroidCompilerContext context, File input) throws IOException {
     synchronized (PREDEX_CACHE) {
       File cacheDir = new File(context.getDexCacheDir());
-      File dexedLib = getDexFileName(input, cacheDir);
+      File cachedDex = getDexFileName(input, cacheDir);
 
-      if (dexedLib.isFile()) {
-        context.getReporter().info(String.format("Using cached: %s <- %s", dexedLib.getName(), input));
-        return dexedLib;
+      if (cachedDex.isFile()) {
+        context.getReporter().info("Using cached: " + cachedDex.getName());
+        return cachedDex;
       }
 
-      boolean success = runR8(context, Collections.singleton(input), null,
-          context.getDexCacheDir(), dexedLib.getName());
+      List<String> cmd = Arrays.asList(
+          "java",
+          "-Xmx" + context.getChildProcessRam() + "M",
+          "-Xss8m",
+          "-cp", context.getResources().getR8Jar(),
+          "com.android.tools.r8.R8",
+          "--release",
+          "--lib", context.getResources().getAndroidRuntime(),
+          "--output", cacheDir.getAbsolutePath(),
+          "--min-api", String.valueOf(AndroidBuildUtils.computeMinSdk(context)),
+          "--no-desugaring",
+          "--no-minification",
+          input.getAbsolutePath()
+      );
 
-      if (!success) {
-        context.getReporter().warn("R8 pre-dex failed, falling back: " + input);
-        return input;
+      context.getReporter().info("Pre-dexing: " + input.getName());
+
+      ByteArrayOutputStream out = new ByteArrayOutputStream();
+      ByteArrayOutputStream err = new ByteArrayOutputStream();
+
+      boolean success = Execution.execute(context.getPaths().getTmpDir(),
+          cmd.toArray(new String[0]),
+          new PrintStream(out), new PrintStream(err),
+          Execution.Timeout.LONG);
+
+      if (success) {
+        File result = new File(cacheDir, "classes.dex");
+        if (result.exists()) {
+          Files.move(result.toPath(), cachedDex.toPath(), StandardCopyOption.REPLACE_EXISTING);
+          return cachedDex;
+        }
       }
 
-      return dexedLib;
+      context.getReporter().warn("R8 pre-dex failed, falling back: " + input);
+      return input;
     }
+  }
+
+  /**
+   * Write ProGuard-style rules to temp file.
+   */
+  private File writeClassRulesToFile(File dir, Set<String> classes) throws IOException {
+    File rules = new File(dir, "main_dex.pro");
+    try (PrintWriter w = new PrintWriter(new FileWriter(rules))) {
+      for (String cls : classes) {
+        w.println("-keep class " + cls + " { *; }");
+      }
+    }
+    return rules;
   }
 }
