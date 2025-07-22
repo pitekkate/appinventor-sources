c86e7f86830530680ac823683bd62253a870d351
pitekkate <kangristiktok@gmail.com>
2025-07-22 16:26:45 +0700

    Update RunR8.java

--- appinventor/buildserver/src/com/google/appinventor/buildserver/tasks/android/RunR8.java
+++ appinventor/buildserver/src/com/google/appinventor/buildserver/tasks/android/RunR8.java
@@ -14,6 +14,7 @@ import com.google.appinventor.buildserver.util.ExecutorUtils;
 
 import java.io.*;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
 @BuildType(aab = true, apk = true)
